3e11c9d2dd204ac0838edaa25ec587d81cb59251
ShuFang <503281@gmail.com>
2022-12-07 18:10:18 +0800

    modified code

--- padim_inference.py
+++ padim_inference.py
@@ -1,21 +1,161 @@
-import torch
-from torchvision.models import resnet18
+import os
+import argparse
+import numpy as np
+import matplotlib.pyplot as plt
+
+import argparse
+import pickle
+from tqdm import tqdm
+from collections import OrderedDict
+from scipy.spatial.distance import mahalanobis
+from scipy.ndimage import gaussian_filter
+import matplotlib
 import random
+import torch
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
 
+from datasets import mvtec
 
-def padim(data_path,checkpoint_pth):
+def padim(data_path,checkpoint_pth,model,device):
 
-    # device setup
-    use_cuda = torch.cuda.is_available()
-    device = torch.device('cuda' if use_cuda else 'cpu')
-
-    # load model
-    model = resnet18(pretrained=False, progress=True)
-    t_d = 448
-    d = 100
-
-    model.to(device)
-    model.eval()
     random.seed(1024)
     torch.manual_seed(1024)
-   
+    use_cuda = torch.cuda.is_available()
+    if use_cuda:
+        torch.cuda.manual_seed_all(1024)
+
+    idx = torch.tensor(random.sample(range(0, t_d), d))
+
+    # set model's intermediate outputs
+    outputs = []
+
+    for class_name in mvtec.CLASS_NAMES:
+
+        train_dataset = mvtec.MVTecDataset(data_path, class_name=class_name, is_train=True)
+        train_dataloader = DataLoader(train_dataset, batch_size=32, pin_memory=True)
+        test_dataset = mvtec.MVTecDataset(data_path, class_name=class_name, is_train=True)
+        test_dataloader = DataLoader(test_dataset, batch_size=32, pin_memory=True)
+        train_outputs = OrderedDict([('layer1', []), ('layer2', []), ('layer3', [])])
+        test_outputs = OrderedDict([('layer1', []), ('layer2', []), ('layer3', [])])
+
+        # extract train set features
+        train_feature_filepath = checkpoint_pth
+        if not os.path.exists(train_feature_filepath):
+            for (x, _, _) in train_dataloader:
+                # model prediction
+                with torch.no_grad():
+                    _ = model(x.to(device))
+                # get intermediate layer outputs
+                for k, v in zip(train_outputs.keys(), outputs):
+                    train_outputs[k].append(v.cpu().detach())
+                # initialize hook outputs
+                outputs = []
+            for k, v in train_outputs.items():
+                train_outputs[k] = torch.cat(v, 0)
+
+            # Embedding concat
+            embedding_vectors = train_outputs['layer1']
+
+            for layer_name in ['layer2', 'layer3']:
+                embedding_vectors = embedding_concat(embedding_vectors, train_outputs[layer_name])
+
+            # randomly select d dimension
+            embedding_vectors = torch.index_select(embedding_vectors, 1, idx)
+            # calculate multivariate Gaussian distribution
+            B, C, H, W = embedding_vectors.size()
+            embedding_vectors = embedding_vectors.view(B, C, H * W)
+            mean = torch.mean(embedding_vectors, dim=0).numpy()
+            cov = torch.zeros(C, C, H * W).numpy()
+            I = np.identity(C)
+            for i in range(H * W):
+                cov[:, :, i] = np.cov(embedding_vectors[:, :, i].numpy(), rowvar=False) + 0.01 * I
+            # save learned distribution
+            train_outputs = [mean, cov]
+            with open(train_feature_filepath, 'wb') as f:
+                pickle.dump(train_outputs, f)
+        else:
+            print('load train set feature from: %s' % train_feature_filepath)
+            with open(train_feature_filepath, 'rb') as f:
+                train_outputs = pickle.load(f)
+
+        test_imgs = []
+        
+        # extract test set features
+        for (x, y, mask) in test_dataloader:
+            test_imgs.extend(x.cpu().detach().numpy())
+            # model prediction
+            with torch.no_grad():
+                _ = model(x.to(device))
+            # get intermediate layer outputs
+            for k, v in zip(test_outputs.keys(), outputs):
+                test_outputs[k].append(v.cpu().detach())
+            # initialize hook outputs
+            outputs = []
+        for k, v in test_outputs.items():
+            test_outputs[k] = torch.cat(v, 0)
+        
+        # Embedding concat
+        embedding_vectors = test_outputs['layer1']
+        for layer_name in ['layer2', 'layer3']:
+            embedding_vectors = embedding_concat(embedding_vectors, test_outputs[layer_name])
+
+        # randomly select d dimension
+        embedding_vectors = torch.index_select(embedding_vectors, 1, idx)
+        
+        # calculate distance matrix
+        B, C, H, W = embedding_vectors.size()
+        embedding_vectors = embedding_vectors.view(B, C, H * W).numpy()
+        dist_list = []
+        for i in range(H * W):
+            mean = train_outputs[0][:, i]
+            conv_inv = np.linalg.inv(train_outputs[1][:, :, i])
+            dist = [mahalanobis(sample[:, i], mean, conv_inv) for sample in embedding_vectors]
+            dist_list.append(dist)
+
+        dist_list = np.array(dist_list).transpose(1, 0).reshape(B, H, W)
+
+        # upsample
+        dist_list = torch.tensor(dist_list)
+        score_map = F.interpolate(dist_list.unsqueeze(1), size=x.size(2), mode='bilinear',
+                                align_corners=False).squeeze().numpy()
+        
+        # apply gaussian smoothing on the score map
+        for i in range(score_map.shape[0]):
+            score_map[i] = gaussian_filter(score_map[i], sigma=4)
+        
+        # Normalization
+        max_score = score_map.max()
+        min_score = score_map.min()
+        scores = (score_map - min_score) / (max_score - min_score)
+        
+        # calculate image-level ROC AUC score
+        return scores
+
+
+def denormalization(x):
+    mean = np.array([0.485, 0.456, 0.406])
+    std = np.array([0.229, 0.224, 0.225])
+    x = (((x.transpose(1, 2, 0) * std) + mean) * 255.).astype(np.uint8)
+    
+    return x
+
+def embedding_concat(x, y):
+    B, C1, H1, W1 = x.size()
+    _, C2, H2, W2 = y.size()
+    s = int(H1 / H2)
+    x = F.unfold(x, kernel_size=s, dilation=1, stride=s)
+    x = x.view(B, C1, -1, H2, W2)
+    z = torch.zeros(B, C1 + C2, x.size(2), H2, W2)
+    for i in range(x.size(2)):
+        z[:, :, i, :, :] = torch.cat((x[:, :, i, :, :], y), 1)
+    z = z.view(B, -1, H2 * W2)
+    z = F.fold(z, kernel_size=s, output_size=(H1, W1), stride=s)
+
+    return z
+
+if __name__ == "__main__":
+
+    img_path = "./result/single/"
+    checkpoint_pth = "./weight/"
+    padim(checkpoint_pth,img_path)
